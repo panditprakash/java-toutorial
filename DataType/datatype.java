7c1785b13c165a25575d1e108d9dd75d13064052
prakashpandit <rajprakash8294@gmial.com>
2025-05-31 14:11:47 +0530

    this is variable

---
+++ DataType/datatype.java
@@ -0,0 +1,19 @@
+package DataType;
+
+public class datatype {
+    int a = 10; // Integer variable
+    double b = 20.5; // Double variable     
+    char c = 'A'; // Character variable
+    String d = "Hello"; // String variable
+    boolean e = true; // Boolean variable
+
+    public static void main(String[] args) {
+        datatype dt = new datatype();
+        System.out.println("Integer: " + dt.a);
+        System.out.println("Double: " + dt.b);
+        System.out.println("Character: " + dt.c);
+        System.out.println("String: " + dt.d);
+        System.out.println("Boolean: " + dt.e);
+    }
+    
+}
---
+++ Variable/vari.java
@@ -0,0 +1,19 @@
+package Variable;
+
+public class vari {
+    int a = 10; // Integer variable
+    double b = 20.5; // Double variable     
+    char c = 'A'; // Character variable
+    String d = "Hello"; // String variable
+    boolean e = true; // Boolean variable
+
+    public static void main(String[] args) {
+        vari var = new vari();
+        System.out.println("Integer: " + var.a);
+        System.out.println("Double: " + var.b);
+        System.out.println("Character: " + var.c);
+        System.out.println("String: " + var.d);
+        System.out.println("Boolean: " + var.e);
+    }
+    
+}
